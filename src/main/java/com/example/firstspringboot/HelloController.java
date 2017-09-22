0dd87a261ce9b7a5a85df0b47eb9fe99046750ae
githubwzh <githubwzh123>
2017-09-22 15:39:43 +0800

    搭建第一个sping boot 程序

---
+++ src/main/java/com/example/firstspringboot/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.firstspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassDescribe:
+ * Author :wangzhanhua
+ * Date: 2017-09-22
+ * Since
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController     //等同于同时加上了@Controller和@ResponseBody
+public class HelloController {
+
+    //访问/hello或者/hi任何一个地址，都会返回一样的结果
+    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
+    public String say(){
+        return "hi you!!!";
+    }
+}
